f813d6efc023a34eb94f5634ec0758d36025c65a
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-24 15:37:44 +0200

    init

---
+++ RoadsOfSpeed/App/RoadsOfSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadsOfSpeedApp.swift
+//  RoadsOfSpeed
+//
+//  Created by Serhii Babchuk on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RoadsOfSpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RSLaunchView()
+        }
+    }
+}
